e0c643c789957493b7eb9bbbebdc223b4aaa4cee
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-12-10 03:06:06 -0800

    Added a long click listener to delete tasks in the ReyclerAdapter

--- app/src/main/java/com/ebookfrenzy/tasksitinerary/RecyclerAdapter.kt
+++ app/src/main/java/com/ebookfrenzy/tasksitinerary/RecyclerAdapter.kt
@@ -8,8 +8,12 @@ import android.widget.ImageView
 import android.view.LayoutInflater
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tasksitinerary.com.ebookfrenzy.tasksitinerary.Task
+import com.example.tasksitinerary.com.ebookfrenzy.tasksitinerary.MyDBHandler
 
-class RecyclerAdapter(private val taskList: List<Task>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+class RecyclerAdapter(
+    private val taskList: MutableList<Task>,
+    private val dbHandler: MyDBHandler
+    ) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
     // ViewHolder to bind task data to views
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -32,6 +36,18 @@ class RecyclerAdapter(private val taskList: List<Task>) : RecyclerView.Adapter<R
         val task = taskList[position]
         holder.taskNameCheckBox.text = task.taskName
         holder.taskImportance.text = task.taskImportance
+
+        // Add long-click listener for deleting the task
+        holder.itemView.setOnLongClickListener {
+            // Delete the task from the database
+            val isDeleted = dbHandler.deleteTask(task.taskName.toString())
+            if (isDeleted) {
+                taskList.removeAt(position)
+                notifyItemRemoved(position)
+                notifyItemRangeChanged(position, taskList.size)
+            }
+            true
+        }
     }
 
     // return the total number of tasks in the array
